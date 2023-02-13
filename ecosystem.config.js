d77803862d03f97a6866a79bc5d18dae61027ca4
ljpson <ljpson@gmail.com>
2023-02-13 15:55:12 +0900

    환경설정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,6 @@ module.exports = {
     apps: [{
         name    : "meta_fashion_api_server",
         script  : "npm",
-        args    : "start",
-        env     : "SERVER_ENV=local"
+        args    : "start"
     }]
 }
